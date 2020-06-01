7802b322e7e69711cf3a08db18cd887eeb71343c
tarun001 <taruntej00001@gmail.com>
2020-06-01 08:23:03 +0530

    Updates

---
+++ precache-manifest.4d98a28291f4b4d8729d51b939ac23a9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b61b3bad42a660e03f181057a9a86b71",
+    "url": "/ta-app/index.html"
+  },
+  {
+    "revision": "8af5a92622b760c18383",
+    "url": "/ta-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "10247e982a2ff89cc324",
+    "url": "/ta-app/static/js/2.c10486fe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ta-app/static/js/2.c10486fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8af5a92622b760c18383",
+    "url": "/ta-app/static/js/main.1a235e72.chunk.js"
+  },
+  {
+    "revision": "9f785b0400418c437a28",
+    "url": "/ta-app/static/js/runtime-main.39ab7aed.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ta-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
